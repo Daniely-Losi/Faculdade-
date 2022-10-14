20f72e211d4c58b2e3d4538bcc264fc53869f5fb
Daniely Losi <112200546+Daniely-Losi@users.noreply.github.com>
2022-10-13 21:12:05 -0300

    Exerc4

---
+++ Pessoa.java
@@ -0,0 +1,59 @@
+package Exerc2;
+
+public class Pessoa {
+	
+	private String nome;
+	private char sexo;
+	private int dataNascimento;
+	private String estadoCivil;
+	public char setSexo;
+	
+	public Pessoa (String nome, char sexo, int dataNascimento) {
+	this.nome = nome;
+	this.sexo = sexo;
+	this.dataNascimento = dataNascimento;
+	this.estadoCivil = "Solteiro"; //padrao//
+	
+	
+}
+
+	public Pessoa() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public char getSexo() {
+      	return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		if((sexo == 'M')|| (sexo ==	'F'))	// M ou F//		
+		this.sexo = sexo;
+	}
+
+	public void  getDataNascimento() {
+		return;
+	}
+
+	public void setDataNascimento(int i) {
+		this.dataNascimento = i;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
+	
+
+
+}                     
---
+++ PessoaTest.java
@@ -0,0 +1,15 @@
+package Exerc2;
+
+public class PessoaTest {
+
+	public static void main(String[] args) {
+		//exemplo
+		Pessoa p1 = new Pessoa();
+		p1.setNome("Daniely");
+		p1.setSexo = 'F'; 
+		p1.setDataNascimento(20/02);
+		p1.setEstadoCivil("Casada");
+	
+	}
+
+}
